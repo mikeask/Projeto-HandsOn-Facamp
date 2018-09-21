bbf49a40986c0893a8e60c75adf40484b2da2269
dequevedo <danielelvisquevedo@gmail.com>
2018-09-20 23:04:57 -0300

    First Animation Concepts

---
+++ Scripts/HumanScripts/HumanController.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _input(event):
+	var animatorBot = $BottomAnimation;
+	var animatorTop = $TopAnimation;
+	
+	if Input.is_action_pressed("ui_right"): animatorBot.play("Idle");
+	
