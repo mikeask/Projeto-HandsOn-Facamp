927a918cd3eadbd39d62f30afdad8b8ad53b14f9
mikeask <31136739+mikeask@users.noreply.github.com>
2018-10-30 19:29:35 -0300

    Preview Inventory (Correção)

--- Scripts/Inventory/GlobalPlayer.gd
+++ Scripts/Inventory/GlobalPlayer.gd
@@ -12,7 +12,9 @@ func loadData():
 	if playerData == null:
 		var dict = {"inventory":{}}
 		for slot in range (0, inventoryMaxSlots):
-			dict["inventory"][String(slot)] = {"id": "0", "amount": 0}
+			if slot==1: dict["inventory"][String(slot)] = {"id": "1", "amount": 1}
+			elif slot==2: dict["inventory"][String(slot)] = {"id": "2", "amount": 10}
+			else: dict["inventory"][String(slot)] = {"id": "0", "amount": 0}
 		GlobalDataParser.writeData(urlPlayerData, dict)
 		inventory = dict["inventory"]
 	else:
