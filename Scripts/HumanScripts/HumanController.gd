d1fa81cc1cf155f3cfb23259aa5c0645018ffbd6
dequevedo <danielelvisquevedo@gmail.com>
2018-10-30 18:44:40 -0300

    Animations Update

--- Scripts/HumanScripts/HumanController.gd
+++ Scripts/HumanScripts/HumanController.gd
@@ -4,6 +4,12 @@ func _input(event):
 	var animatorBot = $BottomAnimation
 	var animatorTop = $TopAnimation
 	
-	if Input.is_action_pressed("ui_right"): 
-		animatorBot.play("Idle")
+	if Input.is_action_pressed("ui_right"):
+		animatorBot.play("WalkSideways")
+	if Input.is_action_pressed("ui_up"):
+		animatorBot.play("WalkForward")
+	if Input.is_action_pressed("ui_left"):
+		animatorBot.play("WalkSideways")
+	if Input.is_action_pressed("ui_down"):
+		animatorBot.play("WalkBackward")
 	
